281ceb746c0d3a469aa4fc003786f44186819e62
blackwut <albertoottimo@gmail.com>
2019-01-04 22:07:36 +0100

    Removed most of pointers

--- fft_audio.c
+++ fft_audio.c
@@ -1,7 +1,11 @@
 #include "fft_audio.h"
+#include <stdlib.h>
 #include <float.h>
-#include <math.h>
-#include <fftw3.h>
+#include <assert.h>
+
+
+#define BEST_BLOCK_SIZE(b) (b > _samplerate ? b : _samplerate) + 1
+#define FREQUENCY_SAMPLE(i) (i * (_samplerate / (float)_block_size))
 
 float * _data = NULL;
 size_t _data_size;
@@ -9,8 +13,8 @@ size_t _samplerate;
 
 /* FFTW stuff */
 fftwf_plan _plan = NULL;
-fftwf_complex * _in;
-fftwf_complex * _out;
+fftwf_complex _in[MAX_BLOCK_SIZE];
+fftwf_complex _out[MAX_BLOCK_SIZE];
 
 /* block variables */
 size_t _block_size;
@@ -18,57 +22,31 @@ size_t _block_offset;
 size_t _bins_size;
 
 
-/* Private functions */
-size_t _fft_audio_best_block_size(size_t block_size) {
-    return block_size + 1;
-}
-
-float _fft_audio_frequency_sample(size_t i)
-{
-    return i * (_samplerate / (float)_block_size);
-}
-
-void _fft_audio_plan_free() {
-    if (_plan) fftwf_destroy_plan(_plan);
-}
-
-void _fft_audio_in_free() {
-    if (_in) fftwf_free(_in);
-}
-
-void _fft_audio_out_free() {
-     if (_out) fftwf_free(_out);
-}
-
 /* Public functions */
-void fft_audio_init(float * data,
+int fft_audio_init(float * data,
                     size_t data_size,
                     size_t samplerate,
                     size_t block_size)
 {
+    assert(data != NULL);
+    assert(data_size > 0);
+    assert(samplerate <= MAX_SAMPLERATE);
+    assert(block_size <= MAX_BLOCK_SIZE);
+
     _data = data;
     _data_size = data_size;
     _samplerate = samplerate;
-    _bins_size = samplerate >> 1;
-    _block_size = (block_size > samplerate) ? block_size : samplerate;
-    _block_size = _fft_audio_best_block_size(_block_size);
+    _bins_size = samplerate / 2 ;
+    _block_size = BEST_BLOCK_SIZE(block_size);
     _block_offset = 0;
-
-    _in = (fftwf_complex *)fftwf_malloc(_block_size * sizeof(fftwf_complex));
-    _out = (fftwf_complex *)fftwf_malloc(_block_size * sizeof(fftwf_complex));
     _plan = fftwf_plan_dft_1d(_block_size, _in, _out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    return FFT_AUDIO_SUCCESS;
 }
 
-fft_audio_block * fft_audio_next_block()
+int fft_audio_next_block(fft_audio_block * block)
 {
-    if (_block_offset + _block_size > _data_size) return NULL;
-
-    float * bins = (float *) calloc(_bins_size, sizeof(float));
-    float max = FLT_MIN;
-    float min = FLT_MAX;
-    float power = 0;
-    float realSum = 0;
-    float imagSum = 0;
+    if (_block_offset + _block_size > _data_size) return -1;
 
     for (size_t i = 0; i < _block_size; ++i) {
         _in[i][0] = _data[_block_offset + i];
@@ -78,13 +56,38 @@ fft_audio_block * fft_audio_next_block()
 
     fftwf_execute(_plan);
 
+    block->size = _block_size;
+    for (size_t i = 0; i < _block_size; ++i) {
+        block->data[i][0] = _out[i][0];
+        block->data[i][1] = _out[i][1];
+    }
+
+    fft_audio_get_stats(&(block->stats), block, 0, _samplerate);
+
+    return FFT_AUDIO_SUCCESS;
+}
+
+int fft_audio_get_stats(fft_audio_stats * stats,
+                        fft_audio_block * block,
+                        size_t freq,
+                        size_t size)
+{
+    assert((freq + size) <= MAX_SAMPLERATE);
+
+    float bins[MAX_SAMPLERATE];
+    float max = FLT_MIN;
+    float min = FLT_MAX;
+    float power = 0;
+    float realSum = 0;
+    float imagSum = 0;
+
     size_t j = 0;
-    for (size_t i = 0; i < _bins_size; ++i) {
+    for (size_t i = freq; i < freq + size; ++i) {
         float magSum = 0.f;
         size_t s = 0;
-        while (j < _block_size >> 1 &&  _fft_audio_frequency_sample(j) <= i) {
-            const float real = _out[j][0];
-            const float imag = _out[j][1];
+        while (FREQUENCY_SAMPLE(j) <= i) {
+            const float real = block->data[j][0];
+            const float imag = block->data[j][1];
             const float val = real * real + imag * imag;
             const float mag = sqrtf(val);
 
@@ -101,26 +104,16 @@ fft_audio_block * fft_audio_next_block()
         bins[i] = magSum / s;
     }
 
-    fft_audio_block * block = (fft_audio_block *) calloc(1, sizeof(fft_audio_block));
-    block->data = bins;
-    block->size = _bins_size;
-    block->max = max;
-    block->min = min;
-    block->power = power;
-    block->amplitude = power / _block_size;
-    block->phase = atanf(imagSum / realSum);
+    stats->max = max;
+    stats->min = min;
+    stats->power = power;
+    stats->amplitude = power / _block_size;
+    stats->phase = atanf(imagSum / realSum);
 
-    return block;
+    return FFT_AUDIO_SUCCESS;
 }
 
-void fft_audio_block_free(fft_audio_block * block)
-{
-    free(block->data);
-    free(block);
-}
-
-void fft_audio_free() {
-    _fft_audio_plan_free();
-    _fft_audio_in_free();
-    _fft_audio_out_free();
+int fft_audio_free() {
+    if (_plan) fftwf_destroy_plan(_plan); // TODO: check what fftwf_destroy_plan returns
+    return FFT_AUDIO_SUCCESS;
 }
--- fft_audio.h
+++ fft_audio.h
@@ -1,27 +1,40 @@
 #ifndef FFT_AUDIO
 #define FFT_AUDIO
 
-#include <stdlib.h>
+#include <math.h>
+#include <fftw3.h>
 
-typedef struct fft_audio_block {
-    float * data;
-    size_t size;
+#define FFT_AUDIO_SUCCESS 0
+
+#define MAX_SAMPLERATE  (44100)
+#define MAX_SECONDS     (10)
+#define MAX_BLOCK_SIZE   (MAX_SAMPLERATE * MAX_SECONDS)
+
+typedef struct fft_audio_stats {
     float max;
     float min;
     float power;
     float amplitude;
     float phase;
+} fft_audio_stats;
+
+typedef struct fft_audio_block {
+    fftwf_complex data[MAX_BLOCK_SIZE];
+    size_t size;
+    fft_audio_stats stats;
+
 } fft_audio_block;
 
-void fft_audio_init(float * data,
-                    size_t data_size,
-                    size_t samplerate,
-                    size_t block_size);
 
-fft_audio_block * fft_audio_next_block();
-
-void fft_audio_block_free(fft_audio_block * block);
-
-void fft_audio_free();
+int fft_audio_init(float * data,
+                   size_t data_size,
+                   size_t samplerate,
+                   size_t block_size);
+int fft_audio_next_block(fft_audio_block * block);
+int fft_audio_get_stats(fft_audio_stats * stats,
+                        fft_audio_block * block,
+                        size_t freq,
+                        size_t size);
+int fft_audio_free();
 
 #endif
