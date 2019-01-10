4f7b39cc4591948118ac9312bea4f8dd289ed4d5
blackwut <albertoottimo@gmail.com>
2019-01-10 16:54:07 +0100

    Added task_handler example and reviewed variable names

--- ptask.c
+++ ptask.c
@@ -23,14 +23,14 @@ pthread_t tid[MAX_TASKS];
 size_t taskCount = 0;
 
 
-int ptask_create(void*(*task)(void *),
+int ptask_create(void * (*task_handler)(void *),
                  const int period,
                  const int deadline,
                  const int priority)
 {
-    pthread_attr_t myatt;
-    struct sched_param mypar;
-    int tret;
+    pthread_attr_t task_attributes;
+    struct sched_param task_sched_params;
+    int ret;
 
     int id = taskCount++;
     assert(id < MAX_TASKS);
@@ -41,52 +41,50 @@ int ptask_create(void*(*task)(void *),
     tp[id].priority = priority;
     tp[id].deadline_miss = 0;
 
-    pthread_attr_init(&myatt);
-    pthread_attr_setinheritsched(&myatt, PTHREAD_EXPLICIT_SCHED);
-    pthread_attr_setschedpolicy(&myatt, SCHED_RR);
-    mypar.sched_priority = tp[id].priority;
-    pthread_attr_setschedparam(&myatt, &mypar);
-    tret = pthread_create(&tid[id], &myatt, task, (void *)(&tp[id]));
+    pthread_attr_init(&task_attributes);
+    pthread_attr_setinheritsched(&task_attributes, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&task_attributes, SCHED_RR);
+    task_sched_params.sched_priority = tp[id].priority;
+    pthread_attr_setschedparam(&task_attributes, &task_sched_params);
+    ret = pthread_create(&tid[id], &task_attributes, task_handler, (void *)(&tp[id]));
 
-    if (tret != 0) return PTASK_ERROR;
+    if (ret != 0) return PTASK_ERROR;
 
     return id;
 }
 
-int ptask_id(void *arg)
+int ptask_id(const void * arg)
 {
-    struct task_par *tp;
-    tp = (struct task_par *)arg;
+    struct task_par * tp = (struct task_par *)arg;
     return tp->id;
 }
 
-void ptask_activate(int id)
+void ptask_activate(const int id)
 {
-    struct timespec t;
-    clock_gettime(CLOCK_MONOTONIC, &t);
-    time_copy(&(tp[id].at), t);
-    time_copy(&(tp[id].dl), t);
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    time_copy(&(tp[id].at), now);
+    time_copy(&(tp[id].dl), now);
     time_add_ms(&(tp[id].at), tp[id].period);
     time_add_ms(&(tp[id].dl), tp[id].deadline);
 }
 
-int ptask_deadline_miss(int id)
+int ptask_deadline_miss(const int id)
 {
     struct timespec now;
     clock_gettime(CLOCK_MONOTONIC, &now);
     if (time_cmp(now, tp[id].dl) > 0) {
         tp[id].deadline_miss++;
-        return 1;
+        return PTASK_DEADLINE_MISS;
     }
     return 0;
 }
 
-void ptask_wait_for_activation(int id)
+void ptask_wait_for_activation(const int id)
 {
     clock_nanosleep_abstime(&(tp[id].at));
     time_add_ms(&(tp[id].at), tp[id].period);
     time_add_ms(&(tp[id].dl), tp[id].deadline);
-    printf("(%d) active!\n", id);
 }
 
 void ptask_wait_tasks()
