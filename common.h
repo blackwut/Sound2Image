fc50ba3337e30ab255b2d85f8fe1b94d2e9e71ce
blackwut <albertoottimo@gmail.com>
2019-01-31 13:48:06 +0100

    Added new constants

--- common.h
+++ common.h
@@ -3,6 +3,32 @@
 
 #include <stdio.h>
 
+/**
+    Constants
+*/
+#define DISPLAY_W               (800)
+#define DISPLAY_H               (600)
+#define FRAMERATE               (30)
+
+#define TASK_INPUT_PERIOD       (1000 / FRAMERATE)
+#define TASK_INPUT_DEADLINE     (1000 / FRAMERATE)
+#define TASK_INPUT_PRIORITY     (99)
+#define TASK_INPUT_QUEUE_TIME   (TASK_DISPLAY_PERIOD - 5)
+
+
+#define TASK_DISPLAY_PERIOD     (1000 / FRAMERATE)
+#define TASK_DISPLAY_DEADLINE   (1000 / FRAMERATE)
+#define TASK_DISPLAY_PRIORITY   (99)
+
+
+#define TASK_FFT_PERIOD         (1000 / FRAMERATE)
+#define TASK_FFT_DEADLINE       (1000 / FRAMERATE)
+#define TASK_FFT_PRIORITY       (49)
+
+#define TASK_BUBBLE_PERIOD       (1000 / FRAMERATE)
+#define TASK_BUBBLE_DEADLINE     (1000 / FRAMERATE)
+#define TASK_BUBBLE_PRIORITY     (49)
+
 #define DEBUG 1
 
 #define EXIT_MALLOC                 1
@@ -12,13 +38,13 @@
 
 
 #define PRINT_TO(file, fmt, ...)\
-fprintf(file, "%s:%d\t"fmt, __func__, __LINE__, __VA_ARGS__)
+    fprintf(file, "%s:%d\t"fmt, __func__, __LINE__, __VA_ARGS__)
 
 #define MLOG(fmt, ...) \
-do { PRINT_TO(stdout, fmt, __VA_ARGS__); } while (0)
+    do { PRINT_TO(stdout, fmt, __VA_ARGS__); } while (0)
 
 #define DLOG(fmt, ...) \
-do { if (DEBUG) PRINT_TO(stderr, fmt, __VA_ARGS__); } while(0)
+    do { if (DEBUG) PRINT_TO(stderr, fmt, __VA_ARGS__); } while(0)
 
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
