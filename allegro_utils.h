789670571162236c66a5f877f3c4202a8bdb142c
blackwut <albertoottimo@gmail.com>
2019-02-14 11:47:29 +0100

    Removed screenshot function, new font, new print_text with alignment

--- allegro_utils.h
+++ allegro_utils.h
@@ -6,17 +6,16 @@
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 
 #include "common.h"
 
-#define FONT_NAME "font/bravani.ttf"
+#define FONT_NAME "font/modum.ttf"
 #define FONT_SIZE 24
 
 
 ALLEGRO_DISPLAY * display = NULL;
-ALLEGRO_COLOR background_color;
+ALLEGRO_COLOR BACKGROUND_COLOR;
 
 ALLEGRO_FONT * font = NULL;
 ALLEGRO_COLOR font_color;
@@ -27,7 +26,7 @@ static inline void al_check(bool test, const char * description)
 {
     if (test) return;
     fprintf(stderr, "ALLEGRO error in %s\n", description);
-    exit(1);
+    exit(EXIT_ALLEGRO_ERROR);
 }
 
 static inline void allegro_init()
@@ -40,11 +39,11 @@ static inline void allegro_init()
     // Display
     al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
     al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
-    al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR | ALLEGRO_MEMORY_BITMAP | ALLEGRO_PIXEL_FORMAT_ANY_WITH_ALPHA);
+    al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR | ALLEGRO_PIXEL_FORMAT_ANY_WITH_ALPHA);
     display = al_create_display(DISPLAY_W, DISPLAY_H);
     al_check(display, "al_create_display()");
     al_register_event_source(queue, al_get_display_event_source(display));
-    background_color = al_map_rgba(0, 0, 0, 255);
+    BACKGROUND_COLOR = al_map_rgba(0, 0, 0, 255);
 
     // Input
     al_check(al_install_keyboard(), "al_install_keyboard()");
@@ -62,13 +61,17 @@ static inline void allegro_init()
     font_color = al_map_rgba(255, 0, 0 , 255);
 
     // Image
-    al_check(al_init_image_addon(), "al_init_image_addon()");
     al_check(al_init_primitives_addon(), "al_init_primitives_addon()");
 }
 
-static inline void allegro_print_text(char const * text, float x, float y)
+ ALLEGRO_CHANNEL_CONF allegro_channel_conf_with(size_t channels)
+ {
+    return (channels == 2 ? ALLEGRO_CHANNEL_CONF_2 : ALLEGRO_CHANNEL_CONF_1);
+ }
+
+static inline void allegro_print_text(const char * text, const float x, const float y, const int align)
 {
-    al_draw_text(font, font_color, x, y, 0, text);
+    al_draw_text(font, font_color, x, y, align, text);
 }
 
 static inline ALLEGRO_BITMAP * allegro_load_bitamp(const char localDirectory[], const char filename[])
@@ -95,86 +98,6 @@ static inline void allegro_unlock(ALLEGRO_BITMAP * bitmap)
     al_unlock_bitmap(bitmap);
 }
 
-static inline int allegro_screenshot(const char * destination_path, const char * gamename)
-{
-    ALLEGRO_PATH *path;
-    char *filename, *filename_wp;
-    struct tm *tmp;
-    time_t t;
-    unsigned int i;
-    const char *path_cstr;
-
-    if(!destination_path)
-        path = al_get_standard_path(ALLEGRO_USER_DOCUMENTS_PATH);
-    else
-        path = al_create_path_for_directory(destination_path);
-
-    if(!path)
-        return -1;
-
-    if(!gamename) {
-        if( !(gamename = al_get_app_name()) ) {
-            al_destroy_path(path);
-            return -2;
-        }
-    }
-
-    t = time(0);
-    tmp = localtime(&t);
-    if(!tmp) {
-        al_destroy_path(path);
-        return -3;
-    }
-
-    // Length of gamename + length of "-YYYYMMDD" + length of maximum [a-z] + NULL terminator
-    if ( !(filename_wp = filename = (char *)malloc(strlen(gamename) + 9 + 2 + 1)) ) {
-        al_destroy_path(path);
-        return -4;
-    }
-
-    strcpy(filename, gamename);
-    // Skip to the '.' in the filename, or the end.
-    for(; *filename_wp != '.' && *filename_wp != 0; ++filename_wp);
-
-    *filename_wp++ = '-';
-    if(strftime(filename_wp, 9, "%Y%m%d", tmp) != 8) {
-        free(filename);
-        al_destroy_path(path);
-        return -5;
-    }
-    filename_wp += 8;
-
-    for(i = 0; i < 26*26; ++i) {
-        if(i > 25) {
-            filename_wp[0] = (i / 26) + 'a';
-            filename_wp[1] = (i % 26) + 'a';
-            filename_wp[2] = 0;
-        }
-        else {
-            filename_wp[0] = (i % 26) + 'a';
-            filename_wp[1] = 0;
-        }
-
-        al_set_path_filename(path, filename);
-        al_set_path_extension(path, ".png");
-        path_cstr = al_path_cstr(path, ALLEGRO_NATIVE_PATH_SEP);
-
-        if (al_filename_exists(path_cstr))
-            continue;
-
-        printf("path: %s\n", path_cstr);
-        al_save_bitmap(path_cstr, al_get_target_bitmap());
-        free(filename);
-        al_destroy_path(path);
-        return 0;
-    }
-
-    free(filename);
-    al_destroy_path(path);
-
-    return -6;
-}
-
 static inline void allegro_free()
 {
     al_uninstall_keyboard();
@@ -182,7 +105,6 @@ static inline void allegro_free()
     al_destroy_font(font);
     al_shutdown_ttf_addon();
     al_shutdown_font_addon();
-    al_shutdown_image_addon();
     al_shutdown_primitives_addon();
     al_destroy_display(display);
     al_destroy_event_queue(queue);
