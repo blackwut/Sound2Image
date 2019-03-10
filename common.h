0944b6a3262ea2a2fcb295ac45c04d34d9ff86a1
blackwut <albertoottimo@gmail.com>
2019-03-10 16:56:42 +0100

    New constant values

--- common.h
+++ common.h
@@ -19,35 +19,34 @@
 #define DISPLAY_AUDIO_H         (DISPLAY_H - (DISPLAY_AUDIO_Y * 2))
 #define FRAMERATE               (50)
 
-// #define INPUT_QUEUE_TIMEOUT     5
-// #define TASK_INPUT_PERIOD       50
-// #define TASK_INPUT_DEADLINE     50
-// #define TASK_INPUT_PRIORITY     (49)
-// #define TASK_INPUT_QUEUE_TIME   (TASK_INPUT_PERIOD - INPUT_QUEUE_TIMEOUT)
+#define TASK_INPUT_PERIOD       30
+#define TASK_INPUT_DEADLINE     30
+#define TASK_INPUT_PRIORITY     30
+#define TASK_INPUT_EVENT_TIME   (20 / 1000.f)
 
 
-#define TASK_DISPLAY_PERIOD     (1000 / FRAMERATE)
-#define TASK_DISPLAY_DEADLINE   (1000 / FRAMERATE)
-#define TASK_DISPLAY_PRIORITY   (49)
+#define TASK_DISPLAY_PERIOD     30
+#define TASK_DISPLAY_DEADLINE   30
+#define TASK_DISPLAY_PRIORITY   30
 
 
-#define TASK_FFT_PERIOD         (1000 / FRAMERATE)
-#define TASK_FFT_DEADLINE       (1000 / FRAMERATE)
-#define TASK_FFT_PRIORITY       (99)
+#define TASK_FFT_PERIOD         20
+#define TASK_FFT_DEADLINE       20
+#define TASK_FFT_PRIORITY       90
 
-#define TASK_BUBBLE_PERIOD       (1000 / FRAMERATE)
-#define TASK_BUBBLE_DEADLINE     (1000 / FRAMERATE)
-#define TASK_BUBBLE_PRIORITY     (69)
+#define TASK_BUBBLE_PERIOD       20
+#define TASK_BUBBLE_DEADLINE     20
+#define TASK_BUBBLE_PRIORITY     60
 
 #define DEBUG 1
 
 #define EXIT_ALLEGRO_ERROR          1
-#define EXIT_FILENAME_NOT_FOUND     2
+#define EXIT_NO_FILENAME_PROVIDED   2
+#define EXIT_OPEN_FILE              3
+#define EXIT_BTRAILS_ERROR          4
+#define EXIT_PTHREAD_CREATE         5
 
-#define EXIT_MALLOC                 1
-#define EXIT_OPEN_SF_FILE           2
-#define EXIT_CLOSE_SF_FILE          3
-#define EXIT_BQUEUE_TIMEOUT_FORMAT  4
+
 
 
 #define MUTEX_LOCK(lock) pthread_mutex_lock(&lock)
