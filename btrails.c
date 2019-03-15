69d3f8194cdc12f9984e4620c068d40baf4f23e4
blackwut <albertoottimo@gmail.com>
2019-03-15 15:24:36 +0100

    Added comments

--- btrails.c
+++ btrails.c
@@ -1,18 +1,34 @@
 #include "btrails.h"
+#include <pthread.h>
 #include <assert.h>
 
-typedef struct {
-	bpoint pos[MAX_BELEMS];
-	size_t top;
-	bcolor color;
-	size_t freq;
 
-	pthread_mutex_t lock;
+//------------------------------------------------------------------------------
+// BTRAILS LOCAL STRUCT DEFINITIONS
+//------------------------------------------------------------------------------
+typedef struct {
+	bpoint pos[MAX_BELEMS];		// (x, y) bubble coordinates of the trail
+
+	size_t top;					// Index of the last bubble coordinates enqueued
+	bcolor color;				// RGB color of the bubbles in the trail
+	size_t freq;				// Frequency assigned to the trail
+
+	pthread_mutex_t lock;		// Lock to protect the trail
 } btrail;
 
+
+//------------------------------------------------------------------------------
+// BRAILS LOCAL DATA
+//------------------------------------------------------------------------------
 static btrail trails[MAX_BTRAILS];
 
 
+//------------------------------------------------------------------------------
+//
+// This function initializes all data required to zero and initializes also the
+// mutex associated to each trail.
+//
+//------------------------------------------------------------------------------
 int btrails_init()
 {
 	size_t i;
@@ -39,12 +55,26 @@ int btrails_init()
 	return BTRAILS_SUCCESS;
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function returns the index of the last bubble enqueued into the
+// specified "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 size_t btrails_get_top(const size_t trail_id)
 {
 	assert(trail_id < MAX_BTRAILS);
 	return trails[trail_id].top;
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function returns a "bpoint" containing the coordinates (x, y) of the 
+// "bubble_id" bubble in the specified "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 bpoint btrails_get_bubble_pos(const size_t trail_id,
 							  const size_t bubble_id)
 {
@@ -53,18 +83,38 @@ bpoint btrails_get_bubble_pos(const size_t trail_id,
 	return trails[trail_id].pos[bubble_id];
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function returns a "bcolor" containing the red, green and blue color
+// channels of a the specified "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 bcolor btrails_get_color(const size_t trail_id)
 {
 	assert(trail_id < MAX_BTRAILS);
 	return trails[trail_id].color;
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function returns the frequency assigned to the "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 size_t btrails_get_freq(const size_t trail_id)
 {
 	assert(trail_id < MAX_BTRAILS);
 	return trails[trail_id].freq;
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function enqueue (possibly overwriting) the bubble coordinates (x, y)
+// into the "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 void btrails_put_bubble_pos(const size_t trail_id,
 							const float x,
 							const float y)
@@ -79,6 +129,13 @@ void btrails_put_bubble_pos(const size_t trail_id,
 	trails[trail_id].pos[next].y = y;
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function assigns the red, green and blue color channels to the
+// "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 void btrails_set_color(const size_t trail_id,
 					   const unsigned char red,
 					   const unsigned char green,
@@ -91,6 +148,12 @@ void btrails_set_color(const size_t trail_id,
 	trails[trail_id].color.blue = blue;
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function assigns the frequency to the "trail_id" trail
+//
+//------------------------------------------------------------------------------
 void btrails_set_freq(const size_t trail_id,
 					  const size_t freq)
 {
@@ -98,11 +161,24 @@ void btrails_set_freq(const size_t trail_id,
 	trails[trail_id].freq = freq;
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function allows to acquire the lock on the specified "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 void btrails_lock(const size_t trail_id)
 {
 	assert(trail_id < MAX_BTRAILS);
 	pthread_mutex_lock(&(trails[trail_id].lock));
 }
+
+
+//------------------------------------------------------------------------------
+//
+// This function allows to release the lock on the specified "trail_id" trail.
+//
+//------------------------------------------------------------------------------
 
 void btrails_unlock(const size_t trail_id)
 {
@@ -110,6 +186,12 @@ void btrails_unlock(const size_t trail_id)
 	pthread_mutex_unlock(&(trails[trail_id].lock));
 }
 
+
+//------------------------------------------------------------------------------
+//
+// This function frees all data and data structures used.
+//
+//------------------------------------------------------------------------------
 void btrails_free()
 {
 	size_t i;
--- btrails.h
+++ btrails.h
@@ -1,17 +1,41 @@
+//------------------------------------------------------------------------------
+//
+// BTRAILS
+//
+// MODULE TO SIMPLIFY THE TRAILS OF BUBBLES.
+//
+// This module provides a simple way to manage the trails of Bubbles
+// implementing an array of circular buffers.
+// All functions are thread UNSAFE, but each operation on a trail can be thread
+// safe calling lock/unlock provided functions.
+//
+//------------------------------------------------------------------------------
 #ifndef BTRAILS_H
 #define BTRAILS_H
 
+
 #include <stdio.h>
-#include <pthread.h>
 
-#define BTRAILS_SUCCESS 0
-#define BTRAILS_ERROR 1
 
-#define MAX_BTRAILS	32
-#define MAX_BELEMS	8
+//------------------------------------------------------------------------------
+// BTRAILS GLOBAL CONSTANTS
+//------------------------------------------------------------------------------
+#define BTRAILS_SUCCESS		0
+#define BTRAILS_ERROR		1
 
+#define MAX_BTRAILS			32
+#define MAX_BELEMS			8
+
+
+//------------------------------------------------------------------------------
+// BRAILS GLOBAL MACROS
+//------------------------------------------------------------------------------
 #define NEXT_BELEM(i) (((i) + 1) % MAX_BELEMS)
 
+
+//------------------------------------------------------------------------------
+// BRAILS GLOBAL STRUCT DEFINITIONS
+//------------------------------------------------------------------------------
 typedef struct {
 	float x;
 	float y;
@@ -23,23 +47,165 @@ typedef struct {
 	unsigned char blue;
 } bcolor;
 
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function initializes all data required.
+//
+// RETURN
+// If not all data required can be initialized, this function returns
+// BTRAILS_ERROR.
+// Otherwise it returns BTRAILS_SUCCESS.
+//
+//------------------------------------------------------------------------------
 int btrails_init();
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function returns the index of the last bubble enqueued into the
+// specified "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+//
+// RETURN
+// The "top" value
+//
+//------------------------------------------------------------------------------
 size_t btrails_get_top(const size_t trail_id);
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function returns a "bpoint" containing the coordinates (x, y) of the 
+// "bubble_id" bubble in the specified "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+// bubble_id: the id of the bubble in the specified trail
+//
+// RETURN
+// The "bpoint" of the specified bubble in the trail
+//
+//------------------------------------------------------------------------------
 bpoint btrails_get_bubble_pos(const size_t trail_id,
 							  const size_t bubble_id);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function returns a "bcolor" containing the red, green and blue color
+// channels of a the specified "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+//
+// RETURN
+// The "bcolor" of the specified trail
+//
+//------------------------------------------------------------------------------
 bcolor btrails_get_color(const size_t trail_id);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function returns the frequency assigned to the "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+//
+// RETURN
+// The frequency assigned to the specified trail
+//
+//------------------------------------------------------------------------------
 size_t btrails_get_freq(const size_t trail_id);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function enqueue (possibly overwriting) the bubble coordinates (x, y)
+// into the "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+// x: the x coordinates
+// y: the y coordinates
+//
+//------------------------------------------------------------------------------
 void btrails_put_bubble_pos(const size_t trail_id,
 							const float x,
 							const float y);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function assigns the red, green and blue color channels to the
+// "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+// red: the red channel of the color
+// green: the green channel of the color
+// blue: the blue channel of the color
+//
+//------------------------------------------------------------------------------
 void btrails_set_color(const size_t trail_id,
 					   const unsigned char red,
 					   const unsigned char green,
 					   const unsigned char blue);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function assigns the frequency to the "trail_id" trail
+//
+// PARAMETERS
+// trail_id: the id of the trail
+// freq: the frequency value to be assigned
+//
+//------------------------------------------------------------------------------
 void btrails_set_freq(const size_t trail_id,
 					  const size_t freq);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function allows to acquire the lock on the specified "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+//
+//------------------------------------------------------------------------------
 void btrails_lock(const size_t trail_id);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function allows to release the lock on the specified "trail_id" trail.
+//
+// PARAMETERS
+// trail_id: the id of the trail
+//
+//------------------------------------------------------------------------------
 void btrails_unlock(const size_t trail_id);
+
+
+//------------------------------------------------------------------------------
+//
+// DESCRIPTION
+// This function frees all data and data structures used.
+//
+//------------------------------------------------------------------------------
 void btrails_free();
+
 
 #endif
